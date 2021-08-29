2aa6bee69d8def16d8e8f9b1675ca6c77f61e8e3
rasindugimhan <83225696+rasindugimhan@users.noreply.github.com>
2021-08-29 18:41:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = express();
 const bot = new TelegramBot(TOKEN, { polling: true } );
 
 const port = process.env.PORT || 5000;
-const gameName = "trexjump";
+const gameName = "T_Rex_Game";
 
 const queries = {};
 
